404f7a13ae821b24b7063192cfd647bceb970209
Eric S. Kubo <erickubo05@gmail.com>
2024-07-03 00:18:33 -0300

    aprendendo condições

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 6.5
+console.log(`A velocidade do seu carro é de ${vel}KM/H`)
+if(vel >= 30){
+    console.log('Você está mutado por ultrapassar o limite de velocidade!')
+} else{
+    console.log('Pode continuar do jeito que está...')
+}
---
+++ aula011/ex009.js
@@ -0,0 +1,6 @@
+var país = 'Alemanha'
+if(país != 'Brasil'){
+    console.log('Você é estrangeiro!')
+} else{
+    console.log('Você é brasileiro!')
+}
